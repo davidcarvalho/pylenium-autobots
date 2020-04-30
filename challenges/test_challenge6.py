1e9578f9a71701b4f16b6ffc9ec099e348672123
david carvalho <david.carvalho@gds.ey.com>
2020-05-01 02:05:57 +0530

    fixed challenge 6 based on suggestion by carlos

--- challenges/test_challenge6.py
+++ challenges/test_challenge6.py
@@ -1,13 +1,14 @@
 from selenium.common.exceptions import TimeoutException
-from selenium.webdriver.common.keys import Keys
 
 
 def test_challenge6(py, copart):
     py.get('#input-search').type('NISSAN')
     py.get('[ng-click="search()"]').click()
-    py.wait(use_py=True).sleep(3)
+    py.get('#serverSideDataTable_processing'). \
+        should().have_attr('style', 'display: block;').should().have_attr('style', 'display: none;')
     py.get('input[type="search"]').type('SKYLINE555')
-    py.wait(use_py=True).sleep(3)
+    py.get('#serverSideDataTable_processing').\
+        should().have_attr('style', 'display: block;').should().have_attr('style', 'display: none;')
     try:
         py.get('tbody span[data-uname="lotsearchLotmodel"]')
     except TimeoutException:
