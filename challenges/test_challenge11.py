a66d714e3f2beefe074449afeb9bf4e07d4829b8
david carvalho <david.carvalho@gds.ey.com>
2020-05-03 20:58:48 +0530

    added link visit to 11

--- challenges/test_challenge11.py
+++ challenges/test_challenge11.py
@@ -3,17 +3,20 @@ import requests
 import re
 
 
-def test_challenge11():
+def test_challenge11(py):
     url = 'https://www.copart.com/'
     headers = {
-        'Cookie': 'G2JSESSIONID=75739723A012C1AFDB92961C94276997-n2;'
-                  'incap_ses_1221_242093=it5QPc9n3n/rPyHyjt3xEOvtrV4AAAAAr+ZIvSAtqf7T4XEXBTw/Ew==;'
+        'Cookie': 'G2JSESSIONID=0181F6E52894C6D4719D580CC5EDB878-n2;'
+                  'incap_ses_1221_242093=cXQzMKUUDnpwmFDyjt3xEPLhrl4AAAAA99VZpDO5D8jOYs/InHdhUA==;'
                   'visid_incap_242093=1jCaSNqwRz6Bj+uncspiZOvtrV4AAAAAQUIPAAAAAADw2ryXoug0vVn6SY6hgdu3;'
-                  'g2usersessionid=e969aeb8b3517b2967639ea6c5ab1a58'
+                  'g2usersessionid=6cfeeb1fc99e48746940221b14e3a43e'
     }
     response = requests.get(url, headers=headers)
     assert response.status_code == 200
     soup = BeautifulSoup(response.content, 'html.parser')
     list_links = []
-    for link in soup.find_all('a', attrs={'href': re.compile("^http")}):
-        list_links.append(link.get('href'))
+    for tag in soup.find_all('a', attrs={'href': re.compile("^http")}):
+        list_links.append(tag.get('href'))
+
+    for link in list_links:
+        py.visit(link)
